e29ba93d86a34f5ab2ba5339691af27750323a33
Karl John Villardar <74708425+kaimatzu@users.noreply.github.com>
2023-12-13 19:46:07 +0800

    Add project files.

---
+++ BPNN/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Geagonia_Bpnn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(139, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(139, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(139, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(139, 119);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.White;
+            this.textBox8.Enabled = false;
+            this.textBox8.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox8.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox8.Location = new System.Drawing.Point(268, 41);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Inputs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Create NN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(33, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Train";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(33, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Feedback:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "?";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 214);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BPNN/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Backprop;
+
+namespace Geagonia_Bpnn
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet Nn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 417;
+            this.Height = 253;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Nn = new NeuralNet(4, 125, 1);
+
+            if (Nn != null)
+            {
+                button1.Enabled = false;
+
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+                textBox8.Enabled = true;
+            }
+                
+
+            label5.Text = "Successfully created neural net!";
+        }
+
+        int ctr = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] inputs = new double[4];
+            double[] desiredOutputs = new double[16];
+            
+            for(int epoch = 0; epoch < 100; epoch++)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    inputs[0] = (i & 1) > 0 ? 1.0 : 0.0;
+                    inputs[1] = (i & 2) > 0 ? 1.0 : 0.0;
+                    inputs[2] = (i & 4) > 0 ? 1.0 : 0.0;
+                    inputs[3] = (i & 8) > 0 ? 1.0 : 0.0;
+
+                    desiredOutputs[i] = i == 15 ? 1.0 : 0.0;
+
+                    Nn.setInputs(inputs);
+                    Nn.setDesiredOutput(0, desiredOutputs[i]);
+                    Nn.learn();
+                }
+                ctr++;
+            }
+
+            label5.Text = $"Successfully trained for {ctr} epochs!";
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 0.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 0.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 0.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 0.0);
+            //Nn.setDesiredOutput(0, 0.0);
+            //Nn.learn();
+
+            //Nn.setInputs(0, 1.0);
+            //Nn.setInputs(1, 1.0);
+            //Nn.setInputs(2, 1.0);
+            //Nn.setInputs(3, 1.0);
+            //Nn.setDesiredOutput(0, 1.0);
+            //Nn.learn();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            //Nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            //Nn.setInputs(2, Convert.ToDouble(textBox3.Text));
+            //Nn.setInputs(3, Convert.ToDouble(textBox4.Text));
+            //Nn.run();
+            //textBox8.Text = "" + Nn.getOuputData(0).ToString("0.000000000000");
+
+            //label5.Text = "Successfully ran logic AND!";
+            try
+            {
+                double input1 = Convert.ToDouble(textBox1.Text);
+                double input2 = Convert.ToDouble(textBox2.Text);
+                double input3 = Convert.ToDouble(textBox3.Text);
+                double input4 = Convert.ToDouble(textBox4.Text);
+
+                Validate(input1);
+                Validate(input2);
+                Validate(input3);
+                Validate(input4);
+
+                Nn.setInputs(0, input1);
+                Nn.setInputs(1, input2);
+                Nn.setInputs(2, input3);
+                Nn.setInputs(3, input4);
+
+                Nn.run();
+
+                textBox8.Text = Nn.getOuputData(0).ToString("0.000000000000");
+              
+                label5.Text = "Successfully ran logic AND!";
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input. Please enter valid numbers.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void Validate(double input)
+        {
+            if (input != 0 && input != 1)
+            {
+                throw new ArgumentException("Invalid input. Please enter either 0 or 1.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = " ";
+            timer1.Stop();
+        }
+    }
+}
